4e7ef4761b46192946e7a6a78b0d9b53050be9c0
notkminq <MinhQuan.T3105@gmail.com>
2025-04-29 17:42:54 +0200

    file reader

--- src/main/java/com/lemms/Main.java
+++ src/main/java/com/lemms/Main.java
@@ -1,9 +1,27 @@
 package com.lemms;
+import java.io.*;
+import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
+        //Quellcode-File auslesen
+        ArrayList<String> lines = new ArrayList<>();
+        InputStream is = Main.class.getClassLoader().getResourceAsStream("exampleCode.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        try {
+
+            String line = null;
+            while((line = br.readLine())!=null){
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(lines); //log source
 
     }
 }
