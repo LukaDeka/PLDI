f2152fed1533af318aad0ba4ea3d0215c6400c06
altay9 <erdemturkoglu40@gmail.com>
2025-06-01 21:23:13 +0200

    lemms api parsing

--- src/main/java/com/lemms/api/LemmsAPI.java
+++ src/main/java/com/lemms/api/LemmsAPI.java
@@ -1,33 +1,40 @@
 package com.lemms.api;
 
+import java.io.File;
 import java.lang.annotation.Native;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.function.Function;
 
+import com.lemms.Parser;
+import com.lemms.Tokenizer;
 import com.lemms.SyntaxNode.StatementNode;
 import com.lemms.interpreter.Interpreter;
 
 public class LemmsAPI {
     private HashMap<String, NativeFunction> nativeFunctions;
-
+    private List<StatementNode> program;
     public void registerFunction(String name, Function<List<Object>, Object> function) {
         nativeFunctions.put(name, function::apply);
 
     }
-    
-    public void setScript(String scriptFilePath) {
 
+    public void setScript(String scriptFilePath) {        
+        String sourcePath = "src/main/resources/example1.1.txt"; // String sourcePath = args[0];
+        File sourceFile = new File(sourcePath);
+        
+        Tokenizer t = new Tokenizer(sourceFile);
+        Parser p = new Parser(t.getTokens());
+        program = p.parse();        
     }
 
-    public void interpret() {
-        List<StatementNode> program = null;
+    public void interpret() {        
         Interpreter interpreter = new Interpreter(program, nativeFunctions);
         interpreter.interpret();
     }
+
     public LemmsAPI() {
         nativeFunctions = new HashMap<>();
     }
 }
-
