df943197e5ea4fba38bda4a1218425bffbcc4cb8
erdemt09 <erdemturkoglu40@gmail.com>
2025-06-17 16:16:54 +0200

    variable nesting

--- src/main/java/com/lemms/SyntaxNode/VariableNode.java
+++ src/main/java/com/lemms/SyntaxNode/VariableNode.java
@@ -6,19 +6,15 @@ import com.lemms.interpreter.ValueVisitor;
 
 public class VariableNode extends ExpressionNode{
     public String name;
-    public Token token;  //Line
-
-    public VariableNode(Token identifierToken) {
-        if (identifierToken == null || identifierToken.getType() != TokenType.IDENTIFIER) {
-            throw new IllegalArgumentException("VariableNode muss mit einem IDENTIFIER-Token erstellt werden.");
-        }
-
-        this.name = identifierToken.getValue();
-        this.token = identifierToken; //Line
-    }
+    public VariableNode child;    
 
     public VariableNode(String identifierName) {
         this.name = identifierName;
+    }
+
+    public VariableNode(String identifierName, VariableNode child) {
+        this.name = identifierName;
+        this.child = child;
     }
 
     @Override
